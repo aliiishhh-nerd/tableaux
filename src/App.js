a522065b8f2865917570eac7b0748a85e1943af7
Alicia <alishinizzle@Alicias-MacBook-Pro.local>
2026-03-25 19:03:06 -0500

    Restore original files with PWA support

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppProvider } from './hooks/useApp';
 import AppShell from './components/AppShell';
-import PWAInstallPrompt from './components/PWAInstallPrompt';
 
 export default function App() {
   return (
     <BrowserRouter>
       <AppProvider>
         <AppShell />
-        <PWAInstallPrompt />
       </AppProvider>
     </BrowserRouter>
   );
