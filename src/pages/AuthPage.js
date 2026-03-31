4021326a3308fd5ed42736018046ed78ffa46332
Alicia <alishinizzle@Alicias-MacBook-Pro.local>
2026-03-30 20:30:38 -0500

    fix: remove unused Link import

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useApp } from '../hooks/useApp';
 
 export default function AuthPage() {
